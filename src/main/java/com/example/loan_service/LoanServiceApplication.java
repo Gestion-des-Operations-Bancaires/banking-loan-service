bf6440b9d9085d7b957b895e0dc374370bdbcd55
Bienvenu2004 <kidsglory1@gmail.com>
2025-05-27 15:41:22 +0100

    feat: added hello endpoint

--- src/main/java/com/example/loan_service/LoanServiceApplication.java
+++ src/main/java/com/example/loan_service/LoanServiceApplication.java
@@ -2,12 +2,21 @@ package com.example.loan_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LoanServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoanServiceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
